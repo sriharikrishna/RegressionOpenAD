63398a7ab3f58663c2c8431f4076b155c6e96ba1
lyonsam <lyonsam@cuban>
2008-11-06 08:36:05 -0600

    removed redundant declaration from an example

--- testRoundTrip/examples/argumentConversion_4e/head.f
+++ testRoundTrip/examples/argumentConversion_4e/head.f
@@ -3,7 +3,6 @@ c$openad XXX Template ad_template.f
 	  double precision, dimension(2) :: x
 	  double precision, dimension(1) :: y
 	  double precision, dimension(2,3) :: t
-	  double precision t
 c$openad INDEPENDENT(x)
 	  do i=1,2
 	    t(i,2)=x(i)
