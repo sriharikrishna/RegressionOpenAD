47e10c9b435e6e5f2d8d699bed394bdc889d3770
m_utkej <m_utkej@utke2.site>
2009-03-31 09:43:14 -0500

    corrected behavior

---
+++ testRoundTrip/examples/allocatedFunction/refOutput/tlmnonehead.pre.xb.x2w.w2f.post.f
@@ -0,0 +1,47 @@
+
+
+      SUBROUTINE head(X, Y)
+      use w2f__types
+      use OAD_active
+      IMPLICIT NONE
+C
+C     **** Global Variables & Derived Type Definitions ****
+C
+      REAL(w2f__8) OpenAD_Symbol_2
+      REAL(w2f__8) OpenAD_Symbol_3
+C
+C     **** Parameters and Result ****
+C
+      type(active) :: X(1:2)
+      type(active) :: Y(1:1)
+C
+C     **** Local Variables and Functions ****
+C
+      type(active) :: A(:)
+      ALLOCATABLE A
+C
+C     **** Top Level Pragmas ****
+C
+C$OPENAD INDEPENDENT(X)
+C$OPENAD DEPENDENT(Y)
+C
+C     **** Statements ****
+C
+C$OPENAD XXX Template ad_template.f
+      ALLOCATE(A(2))
+      A(1)%v = (X(1)%v*2.0D00)
+      CALL sax(2.0D00,X(1),A(1))
+      A(2)%v = (X(2)%v*2.0D00)
+      CALL sax(2.0D00,X(2),A(2))
+      IF(ALLOCATED(A)) THEN
+        Y(1)%v = (A(1)%v*A(2)%v)
+        OpenAD_Symbol_2 = A(2)%v
+        OpenAD_Symbol_3 = A(1)%v
+        CALL sax(OpenAD_Symbol_2,A(1),Y(1))
+        CALL saxpy(OpenAD_Symbol_3,A(2),Y(1))
+      ELSE
+        Y(1)%v = 0
+        CALL zero_deriv(Y(1))
+      ENDIF
+      DEALLOCATE(A)
+      END SUBROUTINE
---
+++ testRoundTrip/examples/allocatedFunction/refOutput/tlmnonehead_sf.pre.xb.x2w.w2f.f
@@ -0,0 +1,54 @@
+
+      MODULE all_globals_mod
+      use w2f__types
+      IMPLICIT NONE
+      SAVE
+C
+C     **** Statements ****
+C
+      END MODULE
+
+      SUBROUTINE head(X, Y)
+      use w2f__types
+      IMPLICIT NONE
+C
+C     **** Global Variables & Derived Type Definitions ****
+C
+      REAL(w2f__8) OpenAD_Symbol_2
+      REAL(w2f__8) OpenAD_Symbol_3
+C
+C     **** Parameters and Result ****
+C
+      TYPE (OpenADTy_active) X(1 : 2)
+      TYPE (OpenADTy_active) Y(1 : 1)
+C
+C     **** Local Variables and Functions ****
+C
+      TYPE (OpenADTy_active) A(:)
+      ALLOCATABLE A
+C
+C     **** Top Level Pragmas ****
+C
+C$OPENAD INDEPENDENT(X)
+C$OPENAD DEPENDENT(Y)
+C
+C     **** Statements ****
+C
+C$OPENAD XXX Template ad_template.f
+      ALLOCATE(A(2))
+      __value__(A(1)) = (__value__(X(1)) * 2.0D00)
+      CALL sax(2.0D00, __deriv__(X(1)), __deriv__(A(1)))
+      __value__(A(2)) = (__value__(X(2)) * 2.0D00)
+      CALL sax(2.0D00, __deriv__(X(2)), __deriv__(A(2)))
+      IF(ALLOCATED(A)) THEN
+        __value__(Y(1)) = (__value__(A(1)) * __value__(A(2)))
+        OpenAD_Symbol_2 = __value__(A(2))
+        OpenAD_Symbol_3 = __value__(A(1))
+        CALL sax(OpenAD_Symbol_2, __deriv__(A(1)), __deriv__(Y(1)))
+        CALL saxpy(OpenAD_Symbol_3, __deriv__(A(2)), __deriv__(Y(1)))
+      ELSE
+        __value__(Y(1)) = 0
+        CALL zero_deriv(__deriv__(Y(1)))
+      ENDIF
+      DEALLOCATE(A)
+      END SUBROUTINE
