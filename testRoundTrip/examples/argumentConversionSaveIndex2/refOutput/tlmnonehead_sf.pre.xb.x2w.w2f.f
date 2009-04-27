fbd4beadbc786eebcaeb1c6e0e326f063bfb1eec
lyonsam <lyonsam@cuban>
2009-04-27 17:10:19 -0500

    new example: argumentConversionSaveIndex2 -- where we do typechange for index expression (i+j) such that only i is overwritten by foo and needs to be saved across

---
+++ testRoundTrip/examples/argumentConversionSaveIndex2/refOutput/tlmnonehead_sf.pre.xb.x2w.w2f.f
@@ -0,0 +1,91 @@
+
+      MODULE all_globals_mod
+      use w2f__types
+      IMPLICIT NONE
+      SAVE
+C
+C     **** Statements ****
+C
+      END MODULE
+
+      MODULE globals
+      use w2f__types
+      IMPLICIT NONE
+      SAVE
+C
+C     **** Global Variables & Derived Type Definitions ****
+C
+      INTEGER(w2f__i4) I
+C
+C     **** Statements ****
+C
+      END MODULE
+
+      SUBROUTINE foo(P)
+      use w2f__types
+      use globals
+      IMPLICIT NONE
+C
+C     **** Global Variables & Derived Type Definitions ****
+C
+      REAL(w2f__8) OpenAD_Symbol_2
+      TYPE (OpenADTy_active) OpenAD_prop_0
+C
+C     **** Parameters and Result ****
+C
+      TYPE (OpenADTy_active) P
+C
+C     **** Statements ****
+C
+      OpenAD_Symbol_2 = (__value__(P) * 2.0D00)
+      __value__(P) = OpenAD_Symbol_2
+      I = (I + 1)
+      CALL setderiv(__deriv__(OpenAD_prop_0), __deriv__(P))
+      CALL sax(2.0D00, __deriv__(OpenAD_prop_0), __deriv__(P))
+      END SUBROUTINE
+
+      SUBROUTINE head(X, Y)
+      use w2f__types
+      use globals
+      IMPLICIT NONE
+C
+C     **** Global Variables & Derived Type Definitions ****
+C
+      TYPE (OpenADTy_active) OpenAD_Symbol_0
+C
+C     **** Parameters and Result ****
+C
+      TYPE (OpenADTy_active) X(1 : 1)
+      INTENT(IN)  X
+      TYPE (OpenADTy_active) Y(1 : 1)
+      INTENT(OUT)  Y
+C
+C     **** Local Variables and Functions ****
+C
+      REAL(w2f__8) A(1 : 1)
+      EXTERNAL foo
+      INTEGER(w2f__i4) J
+      INTEGER(w2f__i8) OpenAD_Symbol_1
+C
+C     **** Top Level Pragmas ****
+C
+C$OPENAD INDEPENDENT(X)
+C$OPENAD DEPENDENT(Y)
+C
+C     **** Statements ****
+C
+C$OPENAD XXX Template ad_template.f
+      I = 1
+      J = 0
+      A(1) = 9.085830301E-03
+      OpenAD_Symbol_1 = I
+C     $OpenAD$ INLINE convert_p2a_scalar(subst,subst)
+      CALL convert_p2a_scalar(__deriv__(OpenAD_Symbol_0), A(J + I))
+      CALL foo(__deriv__(OpenAD_Symbol_0))
+C     $OpenAD$ INLINE convert_a2p_scalar(subst,subst)
+      CALL convert_a2p_scalar(A(J + INT(OpenAD_Symbol_1)), __deriv__(
+     > OpenAD_Symbol_0))
+      CALL foo(__deriv__(X(1)))
+      __value__(Y(1)) = __value__(X(1))
+      CALL setderiv(__deriv__(Y(1)), __deriv__(X(1)))
+      END SUBROUTINE
