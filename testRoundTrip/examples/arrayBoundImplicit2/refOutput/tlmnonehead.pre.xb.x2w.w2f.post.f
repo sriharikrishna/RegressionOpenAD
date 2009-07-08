a0bc32b1a2d14c9b1e6dab4432fa30ff21b8d774
m_utkej <m_utkej@utke2.site>
2009-07-08 11:11:39 -0500

    new test case exercising fix in  OpenADFortTk changeset 7649b683de40

---
+++ testRoundTrip/examples/arrayBoundImplicit2/refOutput/tlmnonehead.pre.xb.x2w.w2f.post.f
@@ -0,0 +1,31 @@
+
+
+      SUBROUTINE head(X, Y)
+      use w2f__types
+      use OAD_active
+      IMPLICIT NONE
+C
+C     **** Parameters and Result ****
+C
+      type(active) :: X(1:2)
+      type(active) :: Y(1:2)
+C
+C     **** Local Variables and Functions ****
+C
+      type(active) :: P(:)
+      ALLOCATABLE P
+C
+C     **** Top Level Pragmas ****
+C
+C$OPENAD INDEPENDENT(X)
+C$OPENAD DEPENDENT(Y)
+C
+C     **** Statements ****
+C
+C$OPENAD XXX Template ad_template.f
+      ALLOCATE(P(3))
+      P(:2)%v = X(1:2)%v
+      CALL setderiv(P(:2),X(1:2))
+      Y(1:2)%v = P(:2)%v
+      CALL setderiv(Y(1:2),P(:2))
+      END SUBROUTINE
