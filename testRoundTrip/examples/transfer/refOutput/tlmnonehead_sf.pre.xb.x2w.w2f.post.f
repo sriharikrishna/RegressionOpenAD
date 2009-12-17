55c5b65e7b169ff7fab4ba5cc01a8b16af167949
m_utkej <m_utkej@utke2.site>
2009-12-17 01:10:52 -0600

    this works ok now after http://mercurial.mcs.anl.gov/ad/OpenADFortTk/rev/5d970ac00776 Note the tolerance in params.conf set to 1.1 because using transfer disables this activity path and so the result is numerically incorrect but entirely expected because of the forced passivation.

---
+++ testRoundTrip/examples/transfer/refOutput/tlmnonehead_sf.pre.xb.x2w.w2f.post.f
@@ -0,0 +1,51 @@
+
+!$OPENAD XXX File_start [OAD_intrinsics.f90]
+MODULE oad_intrinsics
+use w2f__types
+use OAD_active
+IMPLICIT NONE
+SAVE
+!
+!     **** Statements ****
+!
+END MODULE
+
+C$OPENAD XXX File_start [all_globals_mod.f]
+      MODULE all_globals_mod
+      use w2f__types
+      use OAD_active
+      IMPLICIT NONE
+      SAVE
+C
+C     **** Statements ****
+C
+      END MODULE
+
+C$OPENAD XXX File_start [head.f]
+      SUBROUTINE head(X, Y)
+      use w2f__types
+      use OAD_active
+      use oad_intrinsics
+      IMPLICIT NONE
+C
+C     **** Parameters and Result ****
+C
+      type(active) :: X(1:1)
+      type(active) :: Y(1:1)
+C
+C     **** Local Variables and Functions ****
+C
+      INTEGER(w2f__i8) I
+C
+C     **** Top Level Pragmas ****
+C
+C$OPENAD INDEPENDENT(X)
+C$OPENAD DEPENDENT(Y)
+C
+C     **** Statements ****
+C
+C$OPENAD XXX Template ad_template.f
+      I = TRANSFER(X(1)%v,I)
+      Y(1)%v = (X(1)%v+TRANSFER(I,X(1)%v))
+      CALL setderiv(Y(1),X(1))
+      END SUBROUTINE
