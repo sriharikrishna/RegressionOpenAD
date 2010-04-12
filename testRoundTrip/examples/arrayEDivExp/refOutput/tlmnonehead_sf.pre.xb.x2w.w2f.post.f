0326e596f002a312142fe2446cb06eb9f64549f3
m_utkej <m_utkej@utke2.site>
2010-04-12 15:39:42 -0500

    another example

---
+++ testRoundTrip/examples/arrayEDivExp/refOutput/tlmnonehead_sf.pre.xb.x2w.w2f.post.f
@@ -0,0 +1,55 @@
+
+!$OPENAD XXX File_start [OAD_intrinsics.f90]
+MODULE oad_intrinsics
+use w2f__types
+use OAD_active
+IMPLICIT NONE
+SAVE
+!
+!     **** Statements ****
+!
+END MODULE
+
+C$OPENAD XXX File_start [all_globals_mod.f]
+      MODULE all_globals_mod
+      use w2f__types
+      use OAD_active
+      IMPLICIT NONE
+      SAVE
+C
+C     **** Statements ****
+C
+      END MODULE
+
+C$OPENAD XXX File_start [head.f]
+      SUBROUTINE head(X, Y)
+      use w2f__types
+      use OAD_active
+      use oad_intrinsics
+      IMPLICIT NONE
+C
+C     **** Parameters and Result ****
+C
+      type(active) :: X(1:2)
+      type(active) :: Y(1:2)
+C
+C     **** Local Variables and Functions ****
+C
+      REAL(w2f__8) OpenAD_acc_0(1 : 2)
+      REAL(w2f__8) OpenAD_lin_0(1 : 2)
+      REAL(w2f__8) OpenAD_lin_1(1 : 2)
+C
+C     **** Top Level Pragmas ****
+C
+C$OPENAD INDEPENDENT(X)
+C$OPENAD DEPENDENT(Y)
+C
+C     **** Statements ****
+C
+C$OPENAD XXX Template ad_template.f
+      OpenAD_lin_0 = EXP(X%v*5.0D-01)
+      OpenAD_lin_1 = OpenAD_lin_0
+      Y(1:2)%v = OpenAD_lin_0
+      OpenAD_acc_0 = (5.0D-01*OpenAD_lin_1)
+      CALL sax(OpenAD_acc_0,X,Y(1:2))
+      END SUBROUTINE
