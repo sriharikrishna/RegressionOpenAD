891f5058f889dea58c3721052e45bb0cb0eed016
m_utkej <m_utkej@utke2.site>
2010-04-12 15:42:40 -0500

    another example

---
+++ testRoundTrip/examples/arrayEDiv/refOutput/tlmnonehead_sf.pre.xb.x2w.w2f.post.f
@@ -0,0 +1,46 @@
+
+!$OPENAD XXX File_start [OAD_intrinsics.f90]
+MODULE oad_intrinsics
+use w2f__types
+use OAD_active
+IMPLICIT NONE
+SAVE
+!
+!     **** Statements ****
+!
+END MODULE
+
+C$OPENAD XXX File_start [all_globals_mod.f]
+      MODULE all_globals_mod
+      use w2f__types
+      use OAD_active
+      IMPLICIT NONE
+      SAVE
+C
+C     **** Statements ****
+C
+      END MODULE
+
+C$OPENAD XXX File_start [head.f]
+      SUBROUTINE head(X, Y)
+      use w2f__types
+      use OAD_active
+      use oad_intrinsics
+      IMPLICIT NONE
+C
+C     **** Parameters and Result ****
+C
+      type(active) :: X(1:2)
+      type(active) :: Y(1:2)
+C
+C     **** Top Level Pragmas ****
+C
+C$OPENAD INDEPENDENT(X)
+C$OPENAD DEPENDENT(Y)
+C
+C     **** Statements ****
+C
+C$OPENAD XXX Template ad_template.f
+      Y(1:2)%v = (X(1:2)%v*5.0D-01)
+      CALL sax(5.0D-01,X(1:2),Y(1:2))
+      END SUBROUTINE
