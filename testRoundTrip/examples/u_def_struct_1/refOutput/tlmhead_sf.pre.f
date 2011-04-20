8f42baf618bea00e777238b13e8a7714c0538b84
utke on macbook <utke on macbook@localhost>
2011-04-20 17:16:49 -0500

    works after cumulative changes up to http://mercurial.mcs.anl.gov/ad/OpenADFortTk/rev/e219ce510885

--- testRoundTrip/examples/u_def_struct_1/refOutput/tlmhead_sf.pre.f
+++ testRoundTrip/examples/u_def_struct_1/refOutput/tlmhead_sf.pre.f
@@ -1,27 +1,37 @@
-	module myTypeModule
+!$openad xxx file_start [OAD_intrinsics.f90]
+      module OAD_intrinsics
+      end module
+!$openad xxx file_start [all_globals_mod.f]
+      module all_globals_mod
+
+      end module
+
+!$openad xxx file_start [head.f]
+      module myTypeModule
 
         implicit none
         private
         public :: myType
 	
         type myType
-	sequence
-	double precision :: field1 
-	double precision :: field2=0.0
-	end type myType
-	end module
+      sequence
+      double precision :: field1
+      double precision :: field2=0.0
+      end type myType
+      end module
 
-c$openad XXX Template ad_template.f
-	subroutine head(x,y) 
+C$openad XXX Template ad_template.f
+      subroutine head(x,y)
+        use OAD_intrinsics
 	
-	  use myTypeModule
+        use myTypeModule
 
-	  double precision, dimension(2) :: x
-	  double precision, dimension(1) :: y
-	  type(myType) :: typed_x
-c$openad INDEPENDENT(x)
-	  typed_x%field1 = x(1)
-	  typed_x%field2 = x(2)
-          y(1) = typed_x%field1 * typed_x%field2
-c$openad DEPENDENT(y)
-	end subroutine
+        double precision, dimension(2) :: x
+        double precision, dimension(1) :: y
+        type(myType) :: typed_x
+C$openad INDEPENDENT(x)
+        typed_x%field1 = x(1)
+        typed_x%field2 = x(2)
+          y(1) = typed_x%field1*typed_x%field2
+C$openad DEPENDENT(y)
+      end subroutine
